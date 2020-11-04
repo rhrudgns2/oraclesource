4195c9f766b7abb15d8391aa91d160cc2d90d666
kyunghoon <kokd2008@naver.com>
2020-11-04 16:42:26 +0900

    oracle source modified

--- scott.sql
+++ scott.sql
@@ -1,1 +1,12 @@
-select * from tab
+--SCOTT 계정이 가지고 있는 모든 테이블 보기
+select * from tab;
+--select는 조회하는것 뽑아오는 것이라고 생각하면됨
+--emp 테이블의 구조
+DESC emp;
+
+
+--emp 테이블의 모든 내용 조회
+SELECT * FROM emp;
+
+--emp 테이블의 특정필드만 조회
+SELECT ename, job, sal FROM emp;
--- system.sql
+++ system.sql
@@ -1,1 +1,7 @@
-select * from tab
+--scott 계정 암호 지정한 후 계정 해제하기
+
+alter user scott identified by tiger account unlock;
+
+
+--hr
+alter user hr identified by 12345 account unlock;
